60a41c2c85db0f8af74e3d6c8125df78c141a126
Kevin BEAUGRAND <contact@kbeaugrand.fr>
2023-12-12 19:26:48 +0100

    Add code

---
+++ submissions/sk-assistants/samples/01-mathematician/Program.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Kevin BEAUGRAND. All rights reserved.
+
+using _01_mathematician.Plugins;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using SemanticKernel.Assistants;
+
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.development.json", optional: true)
+    .Build();
+
+using var loggerFactory = LoggerFactory.Create(logging =>
+{
+    logging
+        .AddConsole(opts =>
+        {
+            opts.FormatterName = "simple";
+        })
+        .AddConfiguration(configuration.GetSection("Logging"));
+});
+
+string azureOpenAIEndpoint = configuration["AzureOpenAIEndpoint"]!;
+string azureOpenAIKey = configuration["AzureOpenAIAPIKey"]!;
+
+var mathematician = AssistantBuilder.FromTemplate("./Assistants/Mathematician.yaml",
+    azureOpenAIEndpoint,
+    azureOpenAIKey,
+    plugins: new List<IKernelPlugin>()
+    {
+       KernelPluginFactory.CreateFromObject(new MathPlugin(), "math")
+    }, loggerFactory: loggerFactory);
+
+var thread = mathematician.CreateThread();
+
+while (true)
+{
+    Console.Write("User > ");
+    var result = await thread.InvokeAsync(Console.ReadLine());
+    Console.WriteLine($"Mathematician > {result.Content.Trim()}");
+}
